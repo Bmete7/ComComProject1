8d7f7ad9c76a93e61686ce6912e2d7b256a69cd6
Burak Mete <burak.mete11@gmail.com>
2019-03-09 14:39:55 +0300

    client son

--- Client1.py
+++ Client1.py
@@ -7,16 +7,25 @@ serverPort=12000
 clientSocket=socket(AF_INET,SOCK_STREAM)
 
 clientSocket.connect((serverName,serverPort))
+userNameGiven = False
 
 while True:
-	modifiedMessage=clientSocket.recv(1024)
-	print ('This is your question:', modifiedMessage.decode("utf-8"))
-	message=input('What is your answer:')
+    if  (userNameGiven == False):
+        modifiedMessage=clientSocket.recv(1024)
+        print(modifiedMessage.decode("utf-8"))
+        msg=input('')
+        if(msg != ''):
+            userNameGiven = True
+            clientSocket.send(msg.encode())
+    else:
+        modifiedMessage=clientSocket.recv(1024)
+        print ('This is your question:', modifiedMessage.decode("utf-8"))
+        message=input('What is your answer:')
 
-	clientSocket.send(message.encode())
-	if message=="exit":
-		clientSocket.close()
-		exit(0)
-	else :
-		print ("message is sent")
+        clientSocket.send(message.encode())
+        if message=="exit":
+            clientSocket.close()
+            exit(0)
+        else :
+            print ("message is sent")
 
