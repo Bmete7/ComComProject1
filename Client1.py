00b1a31c0bd3606afaf109a6da406bc798f35c32
Burak Mete <burak.mete11@gmail.com>
2019-03-09 12:56:58 +0300

    ilmiye yeni client bu!!!!'

--- Client1.py
+++ Client1.py
@@ -1,84 +1,22 @@
-#TCP socket, threaded server, example 2
-#this server receives a message from client and print it on screen, then wait for another connection requests as well as listen for connected clients
-
+#TCP socket, client, example 2
+#this client connects to the server given with the IP and Port# below and sends a message to server
 from socket import *
-<<<<<<< HEAD
 serverName="192.168.1.190"
 serverPort=12000
-=======
-import threading
 
-class ThreadedServer():
+clientSocket=socket(AF_INET,SOCK_STREAM)
 
+clientSocket.connect((serverName,serverPort))
 
-    def listenToClient(self, client, addr):
-        
-            while True:
+while True:
+	modifiedMessage=clientSocket.recv(1024)
+	print ('This is your question:', modifiedMessage.decode("utf-8"))
+	message=input('What is your answer:')
 
-                    
-                    
-            
-                #question="which version of http is stateless? \n a)http1.0 \n b)http1.1 \n c)both \n d)none"
-                x ="a"
-                client.send(self.questions[1].encode())
-                message= client.recv(1024)
-                if message=="exit":
-                    print (addr , " is closed")
-                    client.close()
-                    exit(0)
-                print("Gelen Cevap:" , message.decode("utf-8"))
-		
+	clientSocket.send(message.encode())
+	if message=="exit":
+		clientSocket.close()
+		exit(0)
+	else :
+		print ("message is sent")
 
-    def __init__(self,serverPort):
-
-        self.questions=["aa","bb","cc","dd","ee"]
-        try:
-            self.serverSocket=socket(AF_INET,SOCK_STREAM)
-
-        except:
-    
-            print ("Socket cannot be created!!!")
-            exit(1)
-            
-        print ("Socket is created...")
-
-        try:
-           self.serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        except:
-    
-            print ("Socket cannot be used!!!")
-            exit(1)
-
-        print ("Socket is being used...")
-
-        try:
-            self.serverSocket.bind(('',serverPort))
-        except:
-        
-            print ("Binding cannot de done!!!")
-            exit(1)
-
-        print ("Binding is done...")
-
-        try:
-            self.serverSocket.listen(45)
-        except:
-    
-            print ("Server cannot listen!!!")
-            exit(1)
->>>>>>> 529cf0721cc645306dd0e8e6e73970eb4b41a084
-
-        print ("The server is ready to receive")
-
-
-        while True:
-
-            connectionSocket,addr=self.serverSocket.accept()
-            
-            threading.Thread(target = self.listenToClient,args = (connectionSocket,addr)).start()
-            
-
-if __name__=="__main__":
-    serverPort=12000
-    ThreadedServer(serverPort)
-	
